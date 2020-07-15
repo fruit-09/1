6d13552b446afd15a450f1a181e9c0ff16d880ab
fruit-09 <68271229+fruit-09@users.noreply.github.com>
2020-07-15 15:09:36 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.swiper-container', {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	//播放过程的速度
+	speed: 1000,
+	autoplay: {
+		delay: 2000,//2秒切换一次
+		disableOnInteraction: false,
+	},//自动滑动
+	
+	//左右箭头
+	// navigation: {
+	// 	nextEl: '.swiper-button-next',
+	// 	prevEl: '.swiper-button-prev',
+	// },
+});
